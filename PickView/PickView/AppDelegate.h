30d679e716b84388e282c022464c838761721fad
赵子辉 <zhaozihui@zhaozihui.local>
2015-11-17 09:16:40 +0800

    first commit

---
+++ PickView/PickView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickView
+//
+//  Created by 赵子辉 on 15/11/17.
+//  Copyright © 2015年 zhaozihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickView/PickView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickView
+//
+//  Created by 赵子辉 on 15/11/17.
+//  Copyright © 2015年 zhaozihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
